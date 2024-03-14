a9a1a1a8c72584df64737ec276ace9ad0bb4a28d
MYCOM <MYCOM@DESKTOP-NAMEDPC>
2024-03-14 10:09:21 +0900

    controller 수정, myinfo 뷰 완성

--- src/main/java/ezen/dteam/controller/Myinfo.java
+++ src/main/java/ezen/dteam/controller/Myinfo.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class MyinfoController {
+public class Myinfo {
 	
 	@RequestMapping(value = "/myinfo", method = RequestMethod.GET)
 	public String myinfo() {
--- src/main/java/ezen/dteam/controller/User.java
+++ src/main/java/ezen/dteam/controller/User.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @RequestMapping(value="/user")
 @Controller
-public class UserController {
+public class User {
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
