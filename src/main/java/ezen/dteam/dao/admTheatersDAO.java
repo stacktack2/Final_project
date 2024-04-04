dde4fb8e133e36c144c486ef8dfa57969289f0db
stacktack <stacktack.2@gmail.com>
2024-04-04 15:54:09 +0900

    커밋커밋

--- src/main/java/ezen/dteam/dao/admTheatersDAO.java
+++ src/main/java/ezen/dteam/dao/admTheatersDAO.java
@@ -1,5 +1,23 @@
 package ezen.dteam.dao;
 
-public class admTheatersDAO {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import ezen.dteam.vo.TheaterVO;
+
+@Repository
+public class admTheatersDAO {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	private final String nameSpace = "ezen.dteam.mapper.admTheatersMapper";
+	
+	public List<TheaterVO> selectAllTheater() {
+		return sqlSession.selectList(nameSpace+".selectAllTheater");
+	}
+	
 }
