12b92ffaca22791b712dde9ed5c65f64bf0c8724
정다희 <@dahee>
2024-04-07 20:48:40 +0900

    오류 커밋

--- bin/target/dflix-1.0.0-BUILD-SNAPSHOT/resources/js/noticeView.js
+++
@@ -1,6 +0,0 @@
-
-function deleteConfirm(){
-	if(confirm("삭제하시겠습니까?")){
-		document.forms['noticeDelete'].submit();
-	}
-}
--- src/main/java/ezen/dteam/dao/MovieDAO.java
+++ src/main/java/ezen/dteam/dao/MovieDAO.java
@@ -1,5 +1,19 @@
 package ezen.dteam.dao;
 
-public class MovieDAO {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import ezen.dteam.vo.CinemaVO;
+
+@Repository
+public class MovieDAO {
+	@Autowired
+	SqlSession sqlSession;
+	
+	private final String nameSpace = "ezen.dteam.mapper.movieMapper";
+	
+	public CinemaVO selectMovieDetail(String ccode){
+		return sqlSession.selectOne(nameSpace+".selectMovieDetail",ccode);
+	}
 }
