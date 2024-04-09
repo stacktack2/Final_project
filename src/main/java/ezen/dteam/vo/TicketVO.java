7def1fd5237ce17a1276a5764091c37e8c76195b
MYCOM <MYCOM@DESKTOP-NAMEDPC>
2024-04-09 09:29:44 +0900

    vo 수정

--- src/main/java/ezen/dteam/vo/TicketVO.java
+++ src/main/java/ezen/dteam/vo/TicketVO.java
@@ -1,5 +1,33 @@
 package ezen.dteam.vo;
 
 public class TicketVO {
-
+	private int ticketno;
+	private String ticketTime;
+	private int ticketDelyn;
+	private int mno;
+	public final int getTicketno() {
+		return ticketno;
+	}
+	public final void setTicketno(int ticketno) {
+		this.ticketno = ticketno;
+	}
+	public final String getTicketTime() {
+		return ticketTime;
+	}
+	public final void setTicketTime(String ticketTime) {
+		this.ticketTime = ticketTime;
+	}
+	public final int getTicketDelyn() {
+		return ticketDelyn;
+	}
+	public final void setTicketDelyn(int ticketDelyn) {
+		this.ticketDelyn = ticketDelyn;
+	}
+	public final int getMno() {
+		return mno;
+	}
+	public final void setMno(int mno) {
+		this.mno = mno;
+	}
+	
 }
